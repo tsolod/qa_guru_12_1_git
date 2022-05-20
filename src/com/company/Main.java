00a5508e64a7954f7ea5446f2484265e187d044a
tsolod <40228890+tsolod@users.noreply.github.com>
2022-05-20 18:09:18 +0300

    added new line flow line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello, qa.guru");
         System.out.println("another line");
+        System.out.println("flow line");
         // write your code here
     }
 }
